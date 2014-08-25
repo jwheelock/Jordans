f753238732059f7c898f3e2a9b8ff6aaa84a990b
Jordan Wheelock <jwheelock@zendesk.com>
2014-08-25 11:36:14 -0500

    Completed JQuery

--- Chapter4/app.js
+++ Chapter4/app.js
@@ -1,5 +1,41 @@
-var main = function () {
+var main = function () 
+{
 	"use strict";
-	window.alert("Hello World");
+
+	$(".comment-input button").on("click", function(event) 
+	{
+		var $new_comment;
+
+		if ($(".comment-input input").val() !== "") 
+	{
+	 	$new_comment = $("<p>").text($(".comment-input input").val());
+		$new_comment.hide();
+		$(".comments").append($new_comment);
+		$new_comment.fadeIn();
+		$(".comment-input input").val("");
+	}
+	else window.alert("MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW");
+});
+
+
+	
+
+	$(".comment-input input").on("keypress", function(event) 
+	{
+		var $new_comment;
+		if (event.keyCode === 13)
+		{
+			if ($(".comment-input input").val() !== "")
+			{
+				$new_comment = $("<p>").text($(".comment-input input").val());
+				$new_comment.hide();
+				$(".comments").append($new_comment);
+				$new_comment.fadeIn();
+				$(".comment-input input").val("");
+			}
+			else window.alert("You must put something in the box, bro.");
+		}
+	});
+
 };
 $(document).ready(main);
